463acedc66f92e1611d068ed31bb4a84b418ec49
skyrocket1000 <77110157+skyrocket1000@users.noreply.github.com>
2022-09-19 13:27:56 +0100

    Add project files.

---
+++ Recursion/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number you want the factorial of: ");
+            Console.WriteLine(FactorialRec(Convert.ToInt32(Console.ReadLine())));
+
+            Console.ReadLine();
+        }
+        static int FactorialRec(int n)
+        {
+
+            if(n == 0)
+            {
+                n = 1;
+            }
+            else
+            {
+                n = n * FactorialRec(n - 1);
+            }
+
+
+
+
+            return n;
+        }
+    }
+}
